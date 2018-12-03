49e0aac996b540e18f5a3a6f7a070c9a801b819f
Hugo Kiiski <kiiskihugo@gmail.com>
2018-12-03 11:57:40 +0200

    Fix meta on mac

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -9,6 +9,8 @@
      (define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
      (define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
      (define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)))
+(setq mac-function-modifier 'meta)
+(setq mac-option-modifier nil)
 
 ;; Org
 (setq org-default-notes-file (expand-file-name "~/Dropbox/org/todo.org"))
