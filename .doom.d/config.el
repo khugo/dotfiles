d71c664a3193077cb2990beee4658057875d6ae3
Hugo Kiiski <kiiskihugo@gmail.com>
2019-02-08 23:14:00 +0200

    Keybind for renaming current file

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -105,7 +105,8 @@
 ;; Keybindings
 (map! :leader
       (:desc "file" :prefix "f"
-        :desc "Find in files"                          :n "s" #'helm-do-ag-project-root)
+        :desc "Find in files"                          :n "s" #'helm-do-ag-project-root
+        :desc "Rename current file"                    :n "n" #'rename-current-buffer-file)
       (:desc "git" :prefix "g"
         :desc "Branch popup"                           :n "b"  #'magit-branch-popup)
       (:desc "open / org" :prefix "o"
@@ -155,3 +156,20 @@
           (set-window-buffer (next-window) next-win-buffer)
           (select-window first-win)
           (if this-win-2nd (other-window 1))))))
+
+(defun rename-current-buffer-file ()
+  "Renames current buffer and file it is visiting"
+  (interactive)
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (error "Buffer '%s' is not visiting a file!" name)
+      (let ((new-name (read-file-name "New name: " filename)))
+        (if (get-buffer new-name)
+            (error "A buffer named '%s' already exists!" new-name)
+          (rename-file filename new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil)
+          (message "File '%s' successfully renamed to '%s'"
+                   name (file-name-nondirectory new-name)))))))
