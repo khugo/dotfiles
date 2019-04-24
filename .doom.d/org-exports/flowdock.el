d0173036774c63e974417ae87155cbbfe8de3d94
Hugo Kiiski <kiiskihugo@gmail.com>
2019-04-24 19:31:00 +0300

    Use same header for level 1 and 2 in flowdock

--- .doom.d/org-exports/flowdock.el
+++ .doom.d/org-exports/flowdock.el
@@ -18,8 +18,7 @@
     (concat (org-flowdock-format-title title level) "\n" contents)))
 
 (defun org-flowdock-format-title (title level)
-  (cond ((= level 1) (concat "\n" title "\n" (make-string (length title) ?=)))
-         ((= level 2) (concat "\n" title "\n" (make-string (length title) ?-)))
+  (cond ((or (= level 1) (= level 2)) (concat "\n" title "\n" (make-string (length title) ?=)))
          ((= level 3) (concat "\n" "**" title "**" "\n"))
          ((>= level 4) (concat (make-string (* (max (- level 4) 0) 2) ? ) "- " title))))
 
