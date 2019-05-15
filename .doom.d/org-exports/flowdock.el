faac99ea4d949bf1ff6d489617aa8c993be82000
Hugo Kiiski <kiiskihugo@gmail.com>
2019-05-15 14:06:08 +0300

    Copy Oskari's flowdock exporter

--- .doom.d/org-exports/flowdock.el
+++ .doom.d/org-exports/flowdock.el
@@ -1,14 +1,16 @@
 (require 'ox)
 
 (org-export-define-backend 'flowdock
-  '((headline . org-flowdock-headline))
+  '((headline . org-flowdock-headline)
+    (paragraph . org-flowdock-paragraph)
+    (plain-text . org-flowdock-plain-text)
+    (section . org-flowdock-section)
+    (link . org-flowdock-link))
   :menu-entry
   '(?f "Export to Flowdock"
        ((?f "To temporary buffer"
-	    (lambda (a s v b) (org-flowdock-export-as-flowdock)))
-	)))
-
-
+            (lambda (a s v b) (org-flowdock-export-as-flowdock)))
+        )))
 
 ;;;; Headline
 
@@ -18,9 +20,33 @@
     (concat (org-flowdock-format-title title level) "\n" contents)))
 
 (defun org-flowdock-format-title (title level)
-  (cond ((or (= level 1) (= level 2)) (concat "\n" title "\n" (make-string (length title) ?=)))
-         ((= level 3) (concat "\n" "**" title "**" "\n"))
-         ((>= level 4) (concat (make-string (* (max (- level 4) 0) 2) ? ) "- " title))))
+  (cond ((= level 1) (concat "\n" title "\n" (make-string (length title) ?=)))
+         ((= level 2) (concat "\n" title "\n" (make-string (length title) ?-)))
+         ((>= level 3) (concat (make-string (* (max (- level 3) 0) 2) ? ) "- " title))))
+
+;;;; Paragraph
+
+(defun org-flowdock-paragraph (paragraph contents info)
+  contents
+  )
+
+;;;; Plain text
+
+(defun org-flowdock-plain-text (text info)
+  text
+  )
+
+;;;; Section
+
+(defun org-flowdock-section (section contents info)
+  contents
+  )
+
+;;;; Link
+
+(defun org-flowdock-link (link desc info)
+   (org-element-property :raw-link link)
+  )
 
 (defun org-flowdock-export-as-flowdock ()
   (interactive)
