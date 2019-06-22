8838b94145815dcccf918653f3331c3e1909151e
Hugo Kiiski <kiiskihugo@gmail.com>
2019-06-22 22:42:40 +0300

    Remove buffer rename function
    There is already another function in my config for this...

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -207,22 +207,6 @@ with a clock from [now - task duration]--[now]."
 (defun org-find-file () (interactive)
   (helm-find-files-1 "~/Dropbox/org/"))
 
-;; source: http://steve.yegge.googlepages.com/my-dot-emacs-file
-(defun rename-file-and-buffer (new-name)
-  "Renames both current buffer and file it's visiting to NEW-NAME."
-  (interactive "sNew name: ")
-  (let ((name (buffer-name))
-        (filename (buffer-file-name)))
-    (if (not filename)
-        (message "Buffer '%s' is not visiting a file!" name)
-      (if (get-buffer new-name)
-          (message "A buffer named '%s' already exists!" new-name)
-        (progn
-          (rename-file filename new-name 1)
-          (rename-buffer new-name)
-          (set-visited-file-name new-name)
-          (set-buffer-modified-p nil))))))
-
 (defun yank-filename ()
   "Put the current file name on the clipboard"
   (interactive)
