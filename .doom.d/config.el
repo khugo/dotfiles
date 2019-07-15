2ce069daa683a2e19bdbeba9396c65a166244db6
Hugo Kiiski <kiiskihugo@gmail.com>
2019-07-15 14:38:55 +0300

    Add WAITING keyword

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -62,8 +62,9 @@
 (setq org-agenda-tag-filter-preset '("-HOME"))
 (setq org-log-done 'time)
 (setq org-agenda-files '("~/Dropbox/org/"))
-(setq org-todo-keywords '((sequence "TODO(t)" "|" "DONE(d)" "CANCELLED(c)")
+(setq org-todo-keywords '((sequence "TODO(t)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)")
                          (sequence "[ ](T)" "|" "[X](D)")))
+(setq org-todo-keyword-faces '(("WAITING" . "light grey")))
 
 (setq org-agenda-start-day "-1d")
 (setq org-agenda-start-on-weekday nil)
